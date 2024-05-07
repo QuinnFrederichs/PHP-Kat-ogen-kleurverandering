eba3b01be6090f4b631eb1a3f43df0686b7d7148
Xenoceph <quinnfrederichs.ks@gmail.com>
2024-05-07 15:50:11 +0200

    Upload Project
    Upload

---
+++ kat pixelart.php
@@ -0,0 +1,110 @@
+<?php
+$color = $_GET['color'] ?? 'white';
+	?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="style.css">
+	<title>Pagina title</title>
+</head>
+<body>
+
+	<div class="grid">
+
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div style="background: <?php echo $color?>;"></div>
+		<div class="pixel"></div>
+		<div style="background: <?php echo $color?>;"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+		<div class="pixel"></div>
+
+	</div>
+
+	<form>
+		<input type="radio" id="red" name="color" value="red">
+		<label for="aqua">Red</label><br>
+		<input type="radio" id="orange" name="color" value="orange">
+		<label for="auqa">Orange</label><br>
+		<input type="radio" id="aqua" name="color" value="aqua">
+		<label for="aqua">Aqua</label>
+		<button type="submit">
+	</form>
+
+
+
+
+</body>
+</html>
